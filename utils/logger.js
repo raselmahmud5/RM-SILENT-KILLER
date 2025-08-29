8bd5a91afcf8ade7c1de345034816767a2728e59
raselmahmud5 <calabobot2@gmail.com>
2025-08-30 01:25:09 +0600

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,1 +1,19 @@
+const fs = require("fs-extra");
+const moment = require("moment");
 
+module.exports = {
+  log(message) {
+    const logMessage = `[${moment().format("YYYY-MM-DD HH:mm:ss")}] ${message}`;
+    console.log(logMessage);
+    fs.appendFileSync("logs/app.log", logMessage + "\n");
+  },
+  error(message) {
+    const errorMessage = `[${moment().format("YYYY-MM-DD HH:mm:ss")}] ERROR: ${message}`;
+    console.error(errorMessage);
+    fs.appendFileSync("logs/error.log", errorMessage + "\n");
+  },
+  event(message) {
+    const eventMessage = `[${moment().format("YYYY-MM-DD HH:mm:ss")}] EVENT: ${message}`;
+    fs.appendFileSync("logs/events.log", eventMessage + "\n");
+  }
+};
