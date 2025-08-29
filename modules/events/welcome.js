d4d5b46ecf9ee5d32d44b2c19b9d27ceec77dd3d
raselmahmud5 <calabobot2@gmail.com>
2025-08-30 02:21:17 +0600

    Update welcome.js

--- modules/events/welcome.js
+++ modules/events/welcome.js
@@ -1,1 +1,8 @@
-
+module.exports = function(api) {
+  api.listenMqtt((err, event) => {
+    if (err) return;
+    if (event.type === "message" && event.body.includes("joined")) {
+      api.sendMessage("Welcome to the group!", event.threadID);
+    }
+  });
+};
