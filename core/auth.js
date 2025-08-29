e42ee4d0dd4f7158744a8668026232425a6e400f
raselmahmud5 <calabobot2@gmail.com>
2025-08-30 01:17:14 +0600

    Update auth.js

--- core/auth.js
+++ core/auth.js
@@ -1,1 +1,11 @@
+const fs = require("fs-extra");
 
+module.exports = {
+  getAppState(path) {
+    if (!fs.existsSync(path)) return null;
+    return fs.readJSONSync(path);
+  },
+  saveAppState(path, appState) {
+    fs.writeJSONSync(path, appState, { spaces: 2 });
+  }
+};
