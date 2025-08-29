36a224f05581de2e9c9ae4e1dd9125d876239c4b
raselmahmud5 <calabobot2@gmail.com>
2025-08-30 02:16:12 +0600

    Update joke.js

--- modules/commands/fun/joke.js
+++ modules/commands/fun/joke.js
@@ -1,1 +1,6 @@
-
+module.exports = {
+  config: { name: "joke", description: "Tell a joke" },
+  run({ api, event }) {
+    api.sendMessage("Why did the chicken cross the road? To get to the other side!", event.threadID);
+  }
+};
