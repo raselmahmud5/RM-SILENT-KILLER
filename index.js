7aa476e9292489a8a252f43de0c2cc93738d525f
raselmahmud5 <calabobot2@gmail.com>
2025-08-30 01:14:50 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const { login } = require("fca-priyansh");
+const loader = require("./core/loader");
+const handler = require("./core/handler");
+const fs = require("fs-extra");
+
+const appStateFile = process.env.APPSTATE_FILE || "./sessions/appstate.json";
+
+login({ appState: fs.readJSONSync(appStateFile) })
+  .then(api => {
+    console.log("Bot logged in!");
+    loader.init(api);
+    handler.start(api);
+  })
+  .catch(err => console.error("Login failed:", err));
