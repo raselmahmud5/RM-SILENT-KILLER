481aa253db949f35ac93057a66842382eccf7936
raselmahmud5 <calabobot2@gmail.com>
2025-08-30 02:10:30 +0600

    Update ban.js

--- modules/commands/admin/ban.js
+++ modules/commands/admin/ban.js
@@ -1,1 +1,6 @@
-
+module.exports = {
+  config: { name: "ban", description: "Ban a user" },
+  run({ api, event, args }) {
+    api.sendMessage(`User ${args[0]} has been banned (simulation).`, event.threadID);
+  }
+};
