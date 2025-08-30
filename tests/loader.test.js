125b85d9ac04bda890acaad413e61e2edc366c78
raselmahmud5 <calabobot2@gmail.com>
2025-08-30 18:47:04 +0600

    Update loader.test.js

--- tests/loader.test.js
+++ tests/loader.test.js
@@ -1,1 +1,14 @@
+const loader = require("../core/loader");
+const assert = require("assert");
 
+describe("Loader Tests", () => {
+  it("should load commands without error", () => {
+    const api = {};
+    assert.doesNotThrow(() => loader.loadCommands(api));
+  });
+
+  it("should load events without error", () => {
+    const api = {};
+    assert.doesNotThrow(() => loader.loadEvents(api));
+  });
+});
