f928abcd641215f5f4c6916e171be508169ae4b8
raselmahmud5 <calabobot2@gmail.com>
2025-08-30 11:13:47 +0600

    Update backup.js

--- scripts/backup.js
+++ scripts/backup.js
@@ -1,1 +1,8 @@
+const fs = require("fs-extra");
+const path = require("path");
 
+const src = path.join(__dirname, "..", "database");
+const dest = path.join(__dirname, "..", "backup", `backup-${Date.now()}`);
+
+fs.copySync(src, dest);
+console.log("✅ Backup completed:", dest);
